4285e55a3f3cce37e1439e3ba5ffd819c5b226a0
Wisleymiguel <wisleymiguel4032x@gmail.com>
2025-09-09 12:55:56 -0300

    tudo ate a aula 03

--- k6-ebac-test/simulations/user.test.js
+++ k6-ebac-test/simulations/user.test.js
@@ -1,7 +1,18 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { group, sleep } from 'k6';
+import Login from '../request/login.request';
+import data from "../data/usuarios.json"
 
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
+
+    let login = new Login()
+
+    group('login and get token', () => {
+      login.access(data.usuarioOk.user,data.usuarioOk.pass)
+    })
+
+    group('list users', () => {
+
+    })
+
 }
