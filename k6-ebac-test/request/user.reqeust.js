13cbd957b05ecdc020239d767220c5fe32c3ef56
Wisleymiguel <wisleymiguel4032x@gmail.com>
2025-09-10 16:54:09 -0300

    colocando tests

---
+++ k6-ebac-test/request/user.reqeust.js
@@ -0,0 +1,19 @@
+import http from "k6/http"
+import Utils from "../utils/utils"
+import { check } from "k6"
+
+export default class User {
+  list(token) {
+    let response = http.get(`${Utils.getBaseUrl()}/users`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    
+    check(response, {
+      'listagem deve retornar 200': r => r.status === 200
+    });
+    
+    return response;
+  }
+}
